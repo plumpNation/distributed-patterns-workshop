3e716618cdffbf23650e90f862303ccce2c83f8c
Gavin King <gavin.king@itslearning.com>
2016-06-05 14:33:08 +0200

    Jonas said I was doing it wrong, because he was doing it wrong

--- 02/host.js
+++ 02/host.js
@@ -5,12 +5,4 @@ const addr = 'tcp://127.0.0.1:7789';
 push.bind(addr);
 
 let n = 1;
-
-let interval = setInterval(function () {
-    if (n === 101) {
-      clearInterval(interval);
-      return;
-    }
-
-    push.send(n++)
-}, 100)
+Array.from(new Array(100), () => setTimeout(() => push.send(n++), 100));
